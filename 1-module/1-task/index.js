4ec8c31e4570be8d59d4aaf77ef160ef400669e1
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-02-05 21:28:29 +0200

    Function that counts factorial of the na=umber

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,18 @@
  * @returns {number}
  */
 function factorial(n) {
+  let result = n;
 
+  if (n === 0 || n === 1) {
+    result = 1;
+  } else if (n === 2) {
+    result = 2;
+  } else {
+    for (i = 1; i < n; i++) {
+      step = n - i;
+      result *= step;
+    }
+  }
+  return result;
 }
+
