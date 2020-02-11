9c5f2d307cfa13fc3a2ef83c13e098795c96cd91
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-02-11 20:49:42 +0200

    Done tasks 3, 4, 5 of the module 1

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,13 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  let length = str.length;
+
+  if (!length) {
+    return str;
+  } else if (length === 1) {
+    return length = str.toUpperCase();
+  } else if (length > 1) {
+    return length = str[0].toUpperCase() + str.slice(1);
+  }
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,10 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-
+  if (str.length > maxlength) {
+    result = str.slice(0, maxlength - 1) + '…';
+    console.log(result);
+  } else {
+    return str;
+  }
 }
