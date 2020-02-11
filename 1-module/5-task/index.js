5d2085df669d94622fbcf615a662789dff54a609
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-02-11 20:59:07 +0200

    update solution for task 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -7,7 +7,7 @@
 function truncate(str, maxlength) {
   if (str.length > maxlength) {
     result = str.slice(0, maxlength - 1) + '…';
-    console.log(result);
+    return result;
   } else {
     return str;
   }
