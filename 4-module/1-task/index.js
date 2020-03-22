46b5abd4c967c1e32c6c4b717e7f9d7e0467c4d2
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-03-22 21:30:08 +0200

    adding 4 module task 1 space

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -9,7 +9,7 @@ function makeFriendsList(friends) {
   for (let item of friends) {
     let li = document.createElement('li');
     li.innerHTML = `${item.firstName} ${item.lastName}`;
-    ul.appendChild(li);
+    ul.appendChild(li); 
   }
 
   return ul;
